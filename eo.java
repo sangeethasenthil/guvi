2b32c7ee555edc690201909c5a0160e87a7e5cd2
sangeethasenthil <sangeethasenthil26071997@gmail.com>
2017-06-05 17:28:02 +0530

    Create eo.java

---
+++ eo.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+class eo
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>=0)
+        {
+           System.out.println("Even");
+        }
+        else
+        {
+            System.out.println("Odd");
+        }
+    }
+}
