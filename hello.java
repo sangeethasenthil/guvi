0404ec0b46f659b32c5285f0df2392fd608b2fe4
sangeethasenthil <sangeethasenthil26071997@gmail.com>
2017-06-06 16:47:21 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class hello
+{
+public static void main(String args[])throws Exception
+{
+Scanner sc=new Scanner(System.in);
+String a=sc.next();
+for(int i=0;i<5;i++)
+{
+System.out.println(a);
+}
+}
+}
